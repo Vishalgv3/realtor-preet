7d7acb2326794811f2605cfb4ff4a2327c9e3224
Vishalgv3 <vishalcanada03@gmail.com>
2024-08-23 14:32:06 -0300

    made a lot of styling changes

--- app/page.js
+++ app/page.js
@@ -5,22 +5,23 @@ import { motion } from "framer-motion"
 
 export default function Home() {
   return (
-    <div className="h-screen bg-cover bg-center bg-black">
+    <div className="relative h-screen bg-cover bg-center bg-[url('/images/Interior-mobile.jpg')] sm:bg-[url('/images/Banglow-day.jpg')]">
+      {/* <div className="absolute inset-0 bg-black opacity-10"></div> */}
 
-      {/* (1) Add a background picture that is darker that the foreground and that covers the entire screen, be it Desktop, laptop, tablet or mobile screen */}
+      {/* (1) Add a background picture that is darker than the foreground and that covers the entire screen, be it Desktop, laptop, tablet or mobile screen */}
 
-      {/* (2) Add an "Hey there, I'm Preet Singh Dhillon, a realtor based out of Truro, Ns."  */}
+      {/* (2) Add an "Hey there, I'm Preet Singh Dhillon, a realtor based out of Truro, Ns."  Done */}
 
       {/* (3) Add Bhaiye's picture right beside the text for desktop screens. */}
 
       {/* (4) Add a block at the bottom of the page that is blackish but not entirely black, and which has bhaiye's social links. */}
 
-      <div className="p-6">
-        <h1 className="text-3xl sm:text-4xl md:text-6xl font-extrabold text-white leading-snug md:mt-0">
+      <div className="p-6 relative z-10 drop-shadow-[0rem_0rem_0.4rem_#000]">
+        <h1 className="text-xl sm:text-2xl md:text-4xl font-extrabold text-white leading-snug md:mt-0">
           Hi, I&apos;m
           <span className="block max-w-fit bg-white rounded px-2 mt-1 italic text-black">Preet Singh Dhillon</span>
         </h1>
-        <motion.p className="text-lg font-extrabold text-white mt-4"
+        <motion.p className="text font-extrabold text-white mt-4"
           initial={{
             opacity: 0,
             y: 25,
@@ -61,7 +62,7 @@ export default function Home() {
             priority
           /> */}
 
-          <span className="mt-8 dark:drop-shadow-[0_0_0.3rem_#ffffff70]">** Replace this button with &quot;Royal Lepage&quot; logo **</span>
+          <span className="mt-8">** Replace this button with &quot;Royal LePage&quot; logo **</span>
 
         </motion.button>
       </div>
