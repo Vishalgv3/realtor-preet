45cb9f1a3d23ee8f1fa1edbb78e607570271fb77
Vishalgv3 <vishalcanada03@gmail.com>
2024-08-23 08:08:50 -0300

    added some comments and framer-motion

--- app/page.js
+++ app/page.js
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="h-screen bg-cover bg-center">
+
+      {/* (1) Add a background picture that is darker that the foreground and that covers the entire screen, be it Desktop, laptop, tablet or mobile screen */}
+
+      {/* (2) Add an "Hey there, I'm Preet Singh Dhillon, a realtor based out of Truro, Ns."  */}
+
+      {/* (3) Add Bhaiye's picture right beside the text for desktop screens. */}
+
+      {/* (4) Add a block at the bottom of the page that is blackish but not entirely black, and which has bhaiye's social links. */}
+
     </div>
   );
 }
